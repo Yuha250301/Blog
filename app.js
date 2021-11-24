89d23e7525cb5c136da2273c86bf9c4dc1df1f87
Huy Trương <dragon1998qn@gmail.com>
2021-11-24 08:25:35 +0700

    Connect DB

--- app.js
+++ app.js
@@ -1,59 +1,83 @@
-const express = require('express');
-const _ = require('lodash');
+const express = require("express");
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const app = new express();
 
 const posts = [];
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        listPost: posts
-    });
-})
+const postSchema = {
+    title: String,
+    content: String,
+};
 
-app.get('/about', (req, res) => {
-    res.render('about');
-})
+const Post = mongoose.model("Blog", postSchema);
+// const About = mongoose.model("About",post);
+// const Contact = mongoose.model("Contact",post);
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
-})
+app.set("view engine", "ejs");
 
-app.get('/compose', (req, res) => {
-    res.render('compose');
-})
-
-app.post('/compose', (req, res) => {
-    const newPost = {
-        title: req.body.newTitle,
-        content: req.body.newContent
-    }
-    posts.push(newPost);
-    res.redirect('/');
-})
-
-app.get('/posts/:postId', (req, res) => {
-    let postFind;
-    const requestTitle = _.lowerCase(req.params.postId);
-
-    for (let index = 0; index < posts.length; index++) {
-        const element = _.lowerCase(posts[index].title);
-        if (element === requestTitle){
-            postFind = posts[index];        
-            break;
+app.get("/", (req, res) => {
+    Post.find({}, (err, posts) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("home", {
+                listPost: posts,
+            });
         }
-    }
-    res.render('post',{post: postFind});
-    
-})
+    });
+});
 
-app.listen('3000', () => {
-    console.log('Stating server at port 3000');
-})
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+app.get("/compose", (req, res) => {
+    res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+    // const newPost = {
+    //     title: req.body.newTitle,
+    //     content: req.body.newContent,
+    // };
+
+    const newPost = new Post({
+        title: req.body.newTitle,
+        content: req.body.newContent,
+    });
+    newPost.save((err) => {
+        if (!err) {res.redirect("/");}
+    });
+    
+});
+
+app.get("/posts/:postId", (req, res) => {
+    const postId = req.params.postId;
+
+    Post.findById(postId, (err, postFind) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("post", { post: postFind });
+        }
+    });
+});
+
+app.listen("3000", () => {
+    console.log("Stating server at port 3000");
+});
